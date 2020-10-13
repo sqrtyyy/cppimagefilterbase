2b2a0c5569694b3f82b6a11090012460bfbe41be
Aleksei <alex.zedfeelgood@gmail.com>
2020-10-13 09:18:00 +0300

    Fixed Threshold

--- Filters/Filter_Threshold.cpp
+++ Filters/Filter_Threshold.cpp
@@ -3,38 +3,14 @@
 //
 
 #include <vector>
-#include <iostream>
 #include "Filter_Threshold.h"
 
 void Filter_Threshold::filter_image(ImageZone &img_data) {
     if (kern.empty()) return;
     Filter_BW filterBw;
     filterBw.filter_image(img_data);
-    unsigned int pixelsInKernLine = img_data.get_width() * kernSize;
-    unsigned int zeroPixelAbscissa[pixelsInKernLine * 2];
-    unsigned int zeroPixelOrdinate[pixelsInKernLine * 2];
-    unsigned int zeroNum1 = 0;
-    unsigned int zeroNum2 = 0;
+    vector<std::pair<unsigned int,unsigned int>> zeroed = {};
     for(unsigned int line = 0; line < img_data.get_height(); line++){
-        if((line + 1) % 5 == 0){
-            unsigned int zeroNum;
-            unsigned int part;
-            if((line + 1) / 5 % 2 == 0){
-                zeroNum = zeroNum2;
-                part = 1;
-                zeroNum2 = 0;
-            } else {
-                zeroNum = zeroNum1;
-                part = 0;
-                zeroNum1 = 0;
-            }
-            part *= pixelsInKernLine;
-            pixel blackPixel = {{0, 0, 0}, 3};
-            for(unsigned int i = 0; i < zeroNum; i++){
-                img_data.set_pixel(zeroPixelOrdinate[i + part], zeroPixelAbscissa[i + part], blackPixel);
-            }
-        }
-
         for(unsigned int column = 0; column < img_data.get_width(); column++){
             int pixelInKern = 0;
             for (int yKern = - (int)(kernSize / 2); yKern <= (int)(kernSize / 2); yKern++) {
@@ -48,39 +24,13 @@ void Filter_Threshold::filter_image(ImageZone &img_data) {
 
             std::nth_element(kern.begin(), kern.begin() + pixelInKern / 2, kern.begin() + pixelInKern);
             if(img_data.get_pixel(line, column).colors[0] < kern[pixelInKern / 2]){
-                unsigned int zeroNum;
-                unsigned int part;
-                if(((line + 1) / 5) % 2 == 1){
-                    zeroNum = zeroNum2;
-                    part = 1;
-                    zeroNum2++;
-                } else {
-                    zeroNum = zeroNum1;
-                    part = 0;
-                    zeroNum1++;
-                }
-                zeroPixelAbscissa[zeroNum + part * pixelsInKernLine] = column;
-                zeroPixelOrdinate[zeroNum + part * pixelsInKernLine] = line;
+               zeroed.emplace_back(line, column);
             }
         }
     }
-    if(img_data.get_height() % 5 != 0){
-        unsigned int zeroNum;
-        unsigned int part;
-        if((img_data.get_height() + 1) / 5 % 2 == 0){
-            zeroNum = zeroNum2;
-            part = 1;
-            zeroNum2 = 0;
-        } else {
-            zeroNum = zeroNum1;
-            part = 0;
-            zeroNum1 = 0;
-        }
-        part *= pixelsInKernLine;
-        pixel blackPixel = {{0, 0, 0}, 3};
-        for(unsigned int i = 0; i < zeroNum; i++){
-            img_data.set_pixel(zeroPixelOrdinate[i + part], zeroPixelAbscissa[i + part], blackPixel);
-        }
+    pixel blackPixel = {{0, 0, 0}, 3};
+    for(auto & coord : zeroed){
+        img_data.set_pixel(coord.first, coord.second, blackPixel);
     }
 }
 
